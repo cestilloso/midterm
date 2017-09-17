a01b047b181f7af957aa29932a5316fcffa6fcba
cestilloso <shessa.estilloso@gmail.com>
2017-09-17 21:06:34 +0800

    commited

--- server.js
+++ server.js
@@ -11,6 +11,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
+app.get('/aswan', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/aswan.html'));
+});
+app.get('/astu', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/astu.html'));
+});
+app.get('/astri', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/astri.html'));
+});
+app.get('/aspor', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/aspor.html'));
+});
+app.get('/contents', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/contents.html'));
+});
+app.get('/grader', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/grader.html'));
+});
+app.get('/incident', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/incident.html'));
+});
+app.get('/last', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/last.html'));
+});
+app.get('/left-sidebar', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/left-sidebar.html'));
+});
+app.get('/no-sidebar', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/no-sidebar.html'));
+});
+app.get('/other', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/other.html'));
+});
+app.get('/right-sidebar', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/right-sidebar.html'));
+});
+app.get('/tech', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/tech.html'));
+});
 
 //express server listen
 var server = app.listen(app.get('port'), function(){
